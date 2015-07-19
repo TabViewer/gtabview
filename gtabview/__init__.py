7644e2804034972efb5001d0edd94e146fed9d4d
Yuri D'Elia <yuri.delia@eurac.edu>
2015-07-19 19:21:25 +0200

    Re-organize in a proper module/script

---
+++ gtabview/__init__.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# gtabview: a simple graphical tabular data viewer
+# Copyright(c) 2014-2015: wave++ "Yuri D'Elia" <wavexx@thregr.org>
+# Copyright(c) 2014-2015: Scott Hansen <firecat four one five three at gmail dot com>
+# Distributed under the MIT license (see LICENSE) WITHOUT ANY WARRANTY.
+from __future__ import print_function, unicode_literals, absolute_import
+
+import io
+import os
+import sys
+import threading
+
+from .viewer import Viewer
+from .viewer import QtGui, QtCore
+
+# Global Viewer for instance recycling
+VIEWER = None
+
+
+# Helper functions
+def _detect_encoding(data=None):
+    """Return the default system encoding. If data is passed, try
+    to decode the data with the default system encoding or from a short
+    list of encoding types to test.
+
+    Args:
+        data - list of lists
+    Returns:
+        enc - system encoding
+    """
+    import locale
+    enc_list = ['utf-8', 'latin-1', 'iso8859-1', 'iso8859-2',
+                'utf-16', 'cp720']
+    code = locale.getpreferredencoding(False)
+    if data is None:
+        return code
+    if code.lower() not in enc_list:
+        enc_list.insert(0, code.lower())
+    for c in enc_list:
+        try:
+            for line in data:
+                line.decode(c)
+        except (UnicodeDecodeError, UnicodeError, AttributeError):
+            continue
+        return c
+    print("Encoding not detected. Please pass encoding value manually")
+
+
+def _parse_lines(data, enc=None, delimiter=None):
+    import csv
+    if enc is None:
+        enc = _detect_encoding(data)
+    if delimiter is None:
+        delimiter = csv.Sniffer().sniff(data[0].decode(enc)).delimiter
+    csv_data = []
+    if sys.version_info.major < 3:
+        csv_obj = csv.reader(data, delimiter=delimiter.encode(enc))
+        for row in csv_obj:
+            row = [str(x, enc) for x in row]
+            csv_data.append(row)
+    else:
+        data = [i.decode(enc) for i in data]
+        csv_obj = csv.reader(data, delimiter=delim)
+        for row in csv_obj:
+            csv_data.append(row)
+    return csv_data
+
+
+def _process_events(widget):
+    while widget.isVisible():
+        QtGui.QApplication.processEvents()
+
+
+def view(data, modal=True, enc=None, start_pos=(0, 0),
+         delimiter=None, hdr_rows=None, recycle=True):
+    # read data into a regular list of lists
+    if isinstance(data, basestring):
+        with open(data, 'rb') as fd:
+            data = _parse_lines(fd.readlines())
+    elif isinstance(data, (io.IOBase, file)):
+        data = _parse_lines(data.readlines())
+
+    # create one application instance if missing
+    if QtGui.qApp is None:
+        QtGui.QApplication([])
+
+    # viewer instance
+    global VIEWER
+    if VIEWER is None or recycle is False:
+        VIEWER = Viewer()
+    view = VIEWER
+    view.view(data, start_pos)
+
+    # run the application loop
+    if modal:
+        view.setWindowModality(QtCore.Qt.ApplicationModal)
+        view.show()
+        _process_events(view)
+    else:
+        view.setWindowModality(QtCore.Qt.NonModal)
+        if view.isVisible():
+            # recycle the previous loop handler
+            pass
+        else:
+            view.show()
+            # we might have more than a single processing/thread loop active
+            # (depending whether we're being used as a stand-alone app or a module
+            # with a QtApp host), but simply, each loop is going to proceed in
+            # lock-step until it's view is closed.
+            thread = threading.Thread(target=_process_events, args=(view,))
+            thread.start()
+        return view
+
---
+++ gtabview/viewer.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import sys
+import threading
+
+# Support PySide/PyQt4 with either Python 2/3
+try:
+    from PySide import QtCore, QtGui
+except ImportError:
+    from PyQt4 import QtCore, QtGui
+if sys.version_info.major < 3:
+    str = unicode
+
+
+class Viewer(QtGui.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(Viewer, self).__init__()
+        self.table = QtGui.QTableWidget()
+        self.setCentralWidget(self.table)
+        self.table.setEditTriggers(QtGui.QTableWidget.EditTrigger.NoEditTriggers)
+        if args or kwargs:
+            self.view(*args, **kwargs)
+
+    def view(self, data, start_pos=None):
+        table = self.table
+        table.clear()
+        table.setSortingEnabled(False)
+        table.sortItems(-1)
+
+        if data.__class__.__name__ in ['Series', 'Panel']:
+            data = data.to_frame()
+        elif isinstance(data, dict):
+            data = [data.keys()] + map(list, zip(*[data[i] for i in data.keys()]))
+
+        if data.__class__.__name__ == 'DataFrame':
+            table.setRowCount(len(data))
+            table.setColumnCount(len(data.columns))
+            table.setHorizontalHeaderLabels(map(str, data.columns.values))
+            table.setVerticalHeaderLabels(map(str, data.index.values))
+            for x, col in enumerate(data):
+                for y in range(len(data)):
+                    widget = QtGui.QTableWidgetItem(str(data.iat[y, x]))
+                    table.setItem(y, x, widget)
+        elif data.__class__.__name__ == 'ndarray':
+            table.setRowCount(data.shape[0])
+            table.setColumnCount(data.shape[1])
+            table.setHorizontalHeaderLabels(map(str, range(data.shape[1])))
+            table.setVerticalHeaderLabels(map(str, range(data.shape[0])))
+            for x in range(data.shape[1]):
+                for y in range(data.shape[0]):
+                    widget = QtGui.QTableWidgetItem(str(data[y, x]))
+                    table.setItem(y, x, widget)
+        elif isinstance(data[0], list):
+            rows = max(1, len(data) - 1)
+            cols = len(data[0])
+            table.setRowCount(rows)
+            table.setColumnCount(cols)
+            table.setHorizontalHeaderLabels(data[0] if len(data) > 1 else
+                                            map(str, range(cols)))
+            table.setVerticalHeaderLabels(map(str, range(rows)))
+            for y, row in enumerate(data[1 if len(data) > 1 else 0:]):
+                for x, cell in enumerate(row):
+                    widget = QtGui.QTableWidgetItem(str(cell))
+                    table.setItem(y, x, widget)
+        else:
+            table.setRowCount(len(data))
+            table.setColumnCount(1)
+            table.setHorizontalHeaderLabels(["list"])
+            table.setVerticalHeaderLabels(map(str, range(len(data))))
+            for y in range(len(data)):
+                widget = QtGui.QTableWidgetItem(str(data[y]))
+                table.setItem(y, 0, widget)
+
+        table.resizeColumnsToContents()
+        table.setSortingEnabled(True)
+        if start_pos:
+            table.setCurrentCell(start_pos[0], start_pos[1])
+
