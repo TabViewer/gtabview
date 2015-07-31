41db7d0ca05994ecd1446a55529c2a7323654b52
Yuri D'Elia <wavexx@thregr.org>
2015-07-31 21:52:54 +0200

    One abs() less.

--- gtabview/viewer.py
+++ gtabview/viewer.py
@@ -366,8 +366,8 @@ class Viewer(QtGui.QMainWindow):
 
         self.table.setFocus()
         if start_pos:
-            y = shape[0] - abs(start_pos[0]) if start_pos[0] < 0 else start_pos[0]
-            x = shape[1] - abs(start_pos[1]) if start_pos[1] < 0 else start_pos[1]
+            y = shape[0] + start_pos[0] if start_pos[0] < 0 else start_pos[0]
+            x = shape[1] + start_pos[1] if start_pos[1] < 0 else start_pos[1]
             self.table.setCurrentIndex(y, x)
 
         self.showNormal()
